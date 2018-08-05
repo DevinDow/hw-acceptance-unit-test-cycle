08c93023c87a71bc761bbdb9d7fde1f531f4b654
Devin Dow <DevinDow@gmail.com>
2018-08-05 14:33:15 +0000

    WIP: route for director

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -1,5 +1,7 @@
 Rottenpotatoes::Application.routes.draw do
+  get 'movies/director', to: 'movies#director', as: 'director'
   resources :movies
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
+  
 end
